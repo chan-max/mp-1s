f06ea81fc6bdc5c783c677d686c0ea002542f7a4
chan-max <jackieontheway666@gmail.com>
2024-05-02 10:38:54 +0800

    setup

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,51 +1,17 @@
 <template>
-	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
-	</view>
+	<div class="index">
+
+	</div>
 </template>
 
-<script lang="ts">
-  import Vue from 'vue';
+<script lang="ts" setup>
 
-	export default Vue.extend({
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	});
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style scoped lang="less">
+.index {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
 </style>
